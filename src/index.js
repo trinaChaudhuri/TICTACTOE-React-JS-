2e2a2ba821665b153e82c06566b9a5aadafb2d9d
User <trinachaudhuri00@gmail.com>
2020-07-26 03:27:45 +0530

    added tic tac toe logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routes from './routes';
+// import Routes from './routes';
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
+import './index.css'
+import Game from './components/Game'
+
+ReactDOM.render(<Game />,document.getElementById("root"))
--- src/screens/StartScreen.js
+++ src/screens/StartScreen.js
@@ -1,11 +1,9 @@
-import React from 'react'
-export default class StartScreen extends React.Component{
-    
-    render(){
-        return(
-            <div>
-                Start
-            </div>
-        )
-    }
+import React from "react";
+import Game from "../components/Game";
+export default class StartScreen extends React.Component {
+  render() {
+    return (
+        <Game />
+    );
+  }
 }
